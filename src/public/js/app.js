dcb320072790a22f87d9304e15aeb2cbb056a723
developer-shkim <developer.shkim@gmail.com>
2021-12-28 22:38:10 +0900

    1.7 Nicknames part One
    - 텍스트를 두 가지 타입(닉네임, 메시지)으로 구분해야 할 필요가 있다.
    - 타입을 지정하기 위해 텍스트를 string 대신 json 형태로 변환한다.
    - 클라이언트 -> 서버로 string 만 전송 가능하므로 json -> string 변환 필요하다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,16 +1,24 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
- 
+
 // app.js 의 socket 은 서버로의 연결을 의미
 // window.location.host 는 우리가 어디에 있는지 알려줌
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
   console.log("Connected to Server ✅")
 })
 
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
@@ -20,8 +28,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = ""; 
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
