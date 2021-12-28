b4f9a651b27459930ed94e360a7d809518e13b50
developer-shkim <developer.shkim@gmail.com>
2021-12-28 19:57:26 +0900

    1.2 WebSockets in NodeJS
    - http 서버 위에 webSockets 서버를 만듦
    - 2개의 프로토콜이 같은 port 를 공유

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -18,4 +20,14 @@ app.get("/", (req, res) => res.render("home"));
 // catch all url, 어떤 url 로 입력하든 / 으로 라우팅된다.
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); 
+// http 서버 만듦
+
+const wss = new WebSocketServer({ server });
+// http 서버 위에 webSocket 서버 만듦
+// 꼭 http 서버 위에 만들 필요는 없다.
+// 이 프로젝트에서 http 가 지원하는 views, static files, home, redirection 을 사용하기 위함
+
+server.listen(3000, handleListen);
+// 2개의 프로토콜이 같은 port 를 공유
