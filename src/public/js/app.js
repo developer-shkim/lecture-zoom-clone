b874ac2c9f2652b0b17835d63696c0166a8d34b7
developer-shkim <developer.shkim@gmail.com>
2021-12-28 20:18:39 +0900

    1.3 WebSocket Events
    - server.js(backend)에 브라우저로 연결하는 socket 생성
    - app.js(frontend)에 서버로 연결하는 socket 생성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,3 @@
-alert("hi"); 
+const socket = new WebSocket(`ws://${window.location.host}`);
+// app.js 의 socket 은 서버로의 연결을 의미
+// window.location.host 는 우리가 어디에 있는지 알려줌
--- src/server.js
+++ src/server.js
@@ -29,5 +29,12 @@ const wss = new WebSocketServer({ server });
 // 꼭 http 서버 위에 만들 필요는 없다.
 // 이 프로젝트에서 http 가 지원하는 views, static files, home, redirection 을 사용하기 위함
 
+function handleConnection(socket) {
+  // server.js 의 socket 은 브라우저로의 연결을 의미
+  console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
 // 2개의 프로토콜이 같은 port 를 공유
