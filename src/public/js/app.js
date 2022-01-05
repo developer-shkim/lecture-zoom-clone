992a65e0dd4a137286ad45f34931df6175e77c76
developer-shkim <developer.shkim@gmail.com>
2022-01-05 22:59:46 +0900

    3.2 Camera Switch
    - 연결되어 있는 비디오 장치 중 사용할 것을 선택할 수 있다.
    - 현재 사용하고 있는 비디오 장치가 selectBox 에서 선택되어 있게 함

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,14 +9,22 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 
-async function getCamera() {
+async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter(device => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+
+
     cameras.forEach(camera => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+
+      if (currentCamera.label === camera.label) {
+        option.selected = true;
+      }
+
       camerasSelect.appendChild(option);
     });
   } catch(e) {
@@ -26,14 +34,25 @@ async function getCamera() {
 
 getMedia();
 
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstraints = {
+    audio: true,
+    video: { fancingMode: "user" },
+  };
+
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId }}
+  };
+
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true, 
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstraints
+    );
     myFace.srcObject = myStream;
-    await getCamera();
+    if (!deviceId) {
+      await getCameras();
+    }
   } catch(e) {
     console.log(e);
   }
@@ -66,5 +85,10 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
+
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
