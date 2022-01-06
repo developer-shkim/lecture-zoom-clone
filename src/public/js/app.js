38525812f4a5f3444f7d4f34f3a1f3d6623a6c39
developer-shkim <developer.shkim@gmail.com>
2022-01-06 23:08:15 +0900

    3.5 Offers
    - offer 를 주고 받기 위해서 서버가 필요하다. (signaling)
    - RTCPeerConnection 으로 전송할 영상, 음성 스트림을 트랙에 추가했다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,6 +13,7 @@ let myStream;
 let muted = false;
 let cameraOff = false;
 let roomName;
+let myPeerConnection;
 
 async function getCameras() {
   try {
@@ -103,10 +104,11 @@ camerasSelect.addEventListener("input", handleCameraChange);
 const welcome = document.getElementById("welcome");
 const welcomeForm = welcome.querySelector("form");
 
-function startMedia() {
+async function startMedia() {
   welcome.hidden = true;
   call.hidden = false;
-  getMedia();
+  await getMedia();
+  makeConnection();
 }
 
 function handleWelcomeSubmit(event) {
@@ -121,6 +123,28 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 
 // Socket Code
 
-socket.on("welcome", () => {
-  console.log("someone joined");
+// Offer 를 주고 받기 위해 서버가 필요하다.
+// 영상, 음성 데이터를 주고 받기 위해서는 서버가 필요없다.
+
+// Peer A, B 가 주고받고 있다고 할 때
+// Peer A 에서 실행
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer"); 
+  socket.emit("offer", offer, roomName);
+});
+
+// Peer B 에서 실행
+socket.on("offer", offer => {
+  console.log(offer);
 })
+
+// RTC Code
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection();
+  myStream
+    .getTracks()
+    .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
