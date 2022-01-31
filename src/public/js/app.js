b4ee4993962dec65e640178fecb2fe6116172907
developer-shkim <developer.shkim@gmail.com>
2022-01-31 21:17:37 +0900

    3.9 STUN
    - 핸드폰과 노트북이 같은 네트워크가 아니라면
    - 연결되지 않는데, 이는 공용주소를 알 수 없기 때문이다.
    - 공용주소를 알려주기 위한 구글 STUN 서버를 추가했다.
    - 실제 서비스를 운영한다면 STUN 서버가 있어야 한다.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -166,9 +166,21 @@ socket.on("ice", ice => {
 })
 
 // RTC Code
-
+// STUN 서버는 나의 장치에 공용주소를 알려주는 서버
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          "stun:stun.l.google.com:19302",
+          "stun:stun1.l.google.com:19302",
+          "stun:stun2.l.google.com:19302",
+          "stun:stun3.l.google.com:19302",
+          "stun:stun4.l.google.com:19302",
+        ]
+      }
+    ]
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream
